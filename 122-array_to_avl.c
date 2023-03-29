e5c2e0fea559ca50dea15c405205827c7d2feab2
girmesh03 <girmachewzewdie03@gmail.com>
2023-03-29 19:39:57 +0300

    112-array_to_bst.c file is added

---
+++ 122-array_to_avl.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * array_to_avl - builds an AVL tree from an array
+ * @array: pointer to the first element of the array to be converted
+ * @size: number of element in the array
+ * Return: a pointer to the root node of the created AVL tree,
+ * or NULL on failure
+ */
+
+avl_t *array_to_avl(int *array, size_t size)
+{
+	avl_t *tree = NULL;
+	size_t i;
+
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		avl_insert(&tree, array[i]);
+
+	return (tree);
+}
