2b9d71efceedddf0525ed5d49ee275f9a0292206
girmesh03 <girmachewzewdie03@gmail.com>
2023-03-30 21:03:23 +0300

    132-array_to_heap.c file updated 3

--- 132-array_to_heap.c
+++ 132-array_to_heap.c
@@ -1,6 +1,31 @@
 #include "binary_trees.h"
 
 /**
+ * tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height
+ *
+ * Return: Height or 0 if tree is NULL
+ */
+size_t tree_height(const heap_t *tree)
+{
+	size_t height_l = 0;
+	size_t height_r = 0;
+
+	if (!tree)
+		return (0);
+
+	if (tree->left)
+		height_l = 1 + tree_height(tree->left);
+
+	if (tree->right)
+		height_r = 1 + tree_height(tree->right);
+
+	if (height_l > height_r)
+		return (height_l);
+	return (height_r);
+}
+
+/**
  * array_to_heap - builds a Max Binary Heap tree from an array
  *
  * @array: pointer to the first element of the array to be converted
