e0164e76a24b4bb67b29f4c4316c3ed864ffb18b
girmesh03 <girmachewzewdie03@gmail.com>
2023-04-02 00:40:07 +0300

    12-binary_tree_leaves.c new update 1

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,16 +8,14 @@
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t leaves = 0;
-
 	if (tree == NULL)
 		return (0);
 
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 
-	leaves = binary_tree_leaves(tree->left);
-	leaves += binary_tree_leaves(tree->right);
+	if (tree->left != NULL || tree->right != NULL)
+		return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 
-	return (leaves);
+	return (0);
 }
