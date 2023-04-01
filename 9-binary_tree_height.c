133397e627cd467a8db26ae06387796b0409331e
girmesh03 <girmachewzewdie03@gmail.com>
2023-04-01 22:02:39 +0300

    9-binary_tree_height.c new update

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,16 +8,16 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height = 0, right_height = 0;
+	size_t left_height, right_height;
 
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left != NULL)
-		left_height = binary_tree_height(tree->left) + 1;
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
 
-	if (tree->right != NULL)
-		right_height = binary_tree_height(tree->right) + 1;
+	left_height = 1 + binary_tree_height(tree->left);
+	right_height = 1 + binary_tree_height(tree->right);
 
 	if (left_height > right_height)
 		return (left_height);
