1ccab783af1a57ea144e3acbff2b8b343d532a99
girmesh03 <girmachewzewdie03@gmail.com>
2023-04-01 21:06:08 +0300

    16-binary_tree_is_perfect.c replaced with new code 1

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -64,23 +64,18 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int left_height, right_height;
-
 	if (tree == NULL)
 		return (0);
 
 	if (binary_tree_is_leaf(tree))
 		return (1);
 
-	if (!binary_tree_is_full(tree))
-		return (0);
-
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-
-	if (left_height != right_height)
-		return (0);
-
-	return (binary_tree_is_perfect(tree->left) &&
-			binary_tree_is_perfect(tree->right));
+	if (tree->left != NULL && tree->right != NULL)
+	{
+		return (binary_tree_is_perfect(tree->left) &&
+				binary_tree_is_perfect(tree->right) &&
+				binary_tree_height(tree->left) ==
+					binary_tree_height(tree->right));
+	}
+	return (0);
 }
