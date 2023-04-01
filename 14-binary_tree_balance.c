c1ae5a7f41092f9bb6ce27b566a09ea985b3424a
girmesh03 <girmachewzewdie03@gmail.com>
2023-04-01 22:22:52 +0300

    14-binary_tree_balance.c new code added and replaced

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -13,11 +13,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left != NULL)
-		left_height = 1 + binary_tree_height(tree->left);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
 
-	if (tree->right != NULL)
-		right_height = 1 + binary_tree_height(tree->right);
+	left_height = 1 + binary_tree_height(tree->left);
+	right_height = 1 + binary_tree_height(tree->right);
 
 	if (left_height > right_height)
 		return (left_height);
@@ -36,6 +36,10 @@ int binary_tree_balance(const binary_tree_t *tree)
 	int left_height = 0, right_height = 0;
 
 	if (tree == NULL)
+		return (0);
+
+	/* balance factor of a leaf node is 0 */
+	if (tree->left == NULL && tree->right == NULL)
 		return (0);
 
 	if (tree->left != NULL)
