33d6477d45a4cdf39f90bdee8d72a8e5718199a1
girmesh03 <girmachewzewdie03@gmail.com>
2023-04-07 18:16:59 +0300

    101-binary_tree_levelorder.c updated with new code 2

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -4,65 +4,91 @@
  * binary_tree_height - measures the height of a binary tree
  * @tree: pointer to the root node of the tree to measure the height.
  * Return: height of the tree
+ *
+ * Note: height of a tree is height of root node, height of a node is the
+ * the longest path from current node to leaf node.
+ * and height of a leaf node is 0
  */
-
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height = 0, right_height = 0;
 
-	if (tree == NULL)
+	/* height of an empty tree or a leaf node is 0 so return 0 */
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0);
 
-	if (tree->left == NULL && tree->right == NULL)
-		return (0);
+	/* else, recursively calculate the height of left and right subtrees */
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-	left_height = 1 + binary_tree_height(tree->left);
-	right_height = 1 + binary_tree_height(tree->right);
-
+	/* return the height of any node except leaf node and empty tree */
+	/* 1 is added to the height of the node to account the current node */
+	/* in which the height is being calculated */
 	if (left_height > right_height)
-		return (left_height);
+		return (1 + left_height);
 	else
-		return (right_height);
+		return (1 + right_height);
 }
 
 /**
- * print_level - prints nodes at a given level
- * @tree: pointer to the root node of the tree to traverse
- * @level: level to print
- * @func: pointer to a function to call for each node
- * Return: nothing
+ * binary_tree_to_array - converts a binary tree to an array
+ * @root: pointer to the root node of the tree to convert
+ * @arr: pointer to the array to store the values
+ * @index: index of the array where the value of the node is to be stored
+ * Return: void
  */
-
-void print_level(const binary_tree_t *tree, void (*func)(int), size_t level)
+void binary_tree_to_array(const binary_tree_t *root, int *arr, int index)
 {
-	if (tree == NULL)
+	/* if root is NULL, return */
+	if (root == NULL)
 		return;
 
-	if (level == 0)
-		func(tree->n);
-	else
-	{
-		print_level(tree->left, func, level - 1);
-		print_level(tree->right, func, level - 1);
-	}
+	/* store the value of the node in the array */
+	arr[index] = root->n;
+
+	/* recursively call the function for left and right child */
+	binary_tree_to_array(root->left, arr, 2 * index + 1);
+	binary_tree_to_array(root->right, arr, 2 * index + 2);
 }
 
 /**
- * binary_tree_levelorder - goes via a binary tree using level-order traversal
+ * binary_tree_levelorder - goes through a binary tree
+ * using level-order traversal
  * @tree: pointer to the root node of the tree to traverse
- * @func: pointer to a function to call for each node
- * Return: nothing
+ * @func: pointer to a function to call for each node.
+ * Return: If tree or func is NULL, do nothing
  */
-
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
-	size_t height = 0, level;
+	int *arr, height, max_nodes, i;
 
+	/* if tree or func is NULL, return */
 	if (tree == NULL || func == NULL)
 		return;
 
 	height = binary_tree_height(tree);
+	/* formula to calculate maximum number */
+	/* of nodes in a binary tree of height h */
+	max_nodes = (1 << (height + 1)) - 1;
 
-	for (level = 0; level <= height; level++)
-		print_level(tree, func, level);
+	/* allocate memory for the array */
+	arr = malloc(max_nodes * sizeof(int));
+	if (arr == NULL)
+		return;
+
+	/* initialize the array with -1 */
+	for (i = 0; i < max_nodes; i++)
+		arr[i] = -1;
+
+	/* convert the binary tree to an array */
+	binary_tree_to_array(tree, arr, 0);
+
+	/* call the function for each node in the array */
+	for (i = 0; i < max_nodes; i++)
+	{
+		if (arr[i] != -1)
+			func(arr[i]);
+	}
+	/* free the allocated memory */
+	free(arr);
 }
