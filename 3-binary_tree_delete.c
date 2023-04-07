c9d5efb810efacb56cd382e34d712608bf53f0a2
girmesh03 <girmachewzewdie03@gmail.com>
2023-04-07 10:50:25 +0300

    3-binary_tree_delete.c updated with new code 1

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -6,25 +6,14 @@
  * @tree: Pointer to the root node of the tree to delete
  * Return: void
  */
-
 void binary_tree_delete(binary_tree_t *tree)
 {
+	/* Base case: If tree is empty, return */
 	if (tree == NULL)
 		return;
-
-	/* Recursively delete the left and right subtrees */
+	/* Delete the left and right subtrees */
 	binary_tree_delete(tree->left);
 	binary_tree_delete(tree->right);
-
-	/* Once the subtrees are deleted, delete the current node */
-	if (tree->parent != NULL)
-	{
-		/* Update the parent's left or right pointer, if necessary */
-		if (tree->parent->left == tree)
-			tree->parent->left = NULL;
-		else
-			tree->parent->right = NULL;
-	}
-
+	/* Delete the root node or parent node */
 	free(tree);
 }
