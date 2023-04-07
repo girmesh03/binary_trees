37a3c390eff50bfae36bae42f453593f6ff4a9f9
girmesh03 <girmachewzewdie03@gmail.com>
2023-04-07 08:53:15 +0300

    12-binary_tree_leaves.c updated with new code 1

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,14 +8,14 @@
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+	/* If tree is empty, return 0 */
 	if (tree == NULL)
 		return (0);
-
+	/* If tree is a leaf, increment leaf node count by 1 */
+	/* and recursively check the remaining left and right subtrees */
 	if (tree->left == NULL && tree->right == NULL)
-		return (1);
-
-	if (tree->left != NULL || tree->right != NULL)
-		return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
-
-	return (0);
+		return (1 + (binary_tree_leaves(tree->left) +
+					 binary_tree_leaves(tree->right)));
+	/* otherwise, recursively check the left and right subtrees */
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
