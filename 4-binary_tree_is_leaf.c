a6115a2f0ae5d8e7d0aa7f43fb51eab2bc7f596b
girmesh03 <girmachewzewdie03@gmail.com>
2023-04-07 11:04:04 +0300

    4-binary_tree_is_leaf.c updated with new code 1

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -5,10 +5,12 @@
  * @node: pointer to the node to check
  * Return: 1 if node is a leaf, otherwise 0
  */
-
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
+	/* empty node can't be a leaf node so return 0 */
 	if (node == NULL)
 		return (0);
+	/* otherwise, if node exists but has no children, return 1 */
+	/* because it is a leaf node */
 	return (node->left == NULL && node->right == NULL);
 }
