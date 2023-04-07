039b07ff717e32d9974d324f16a6626b92d6feda
girmesh03 <girmachewzewdie03@gmail.com>
2023-04-07 12:50:13 +0300

    13-binary_tree_nodes.c updated with new code 1

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,37 +1,18 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_leaf - checks if a node is a leaf
- * @node: pointer to the node to check
- * Return: 1 if node is a leaf, otherwise 0
- */
-
-int binary_tree_is_leaf(const binary_tree_t *node)
-{
-	if (node == NULL)
-		return (0);
-	return (node->left == NULL && node->right == NULL);
-}
-
-/**
  * binary_tree_nodes - counts the nodes with at least 1 child in a binary tree
  * @tree: pointer to the root node of the tree to count the number of nodes
  * Return: number of nodes
  */
-
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	if (tree == NULL)
+	/* If tree is empty, or a leaf node then return 0 */
+	/* b/c we are only counting nodes with 1 or 2 children */
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0);
-
-	/* if the current node is not a leaf, then it has at least one child */
-	/* so add 1 to the number of nodes and */
-	/* call recursively on the left and right */
-	/* then return the number of nodes */
-	if (!(binary_tree_is_leaf(tree)))
-		return (1 + (binary_tree_nodes(tree->left) +
-					 binary_tree_nodes(tree->right)));
-
-	/* otherwise, the current node is a leaf, which has no children */
-	return (0);
+	/* otherwise, recursively return the sum of number of nodes in left and */
+	/* right subtrees and the current node */
+	return (1 + (binary_tree_nodes(tree->left) +
+				 binary_tree_nodes(tree->right)));
 }
