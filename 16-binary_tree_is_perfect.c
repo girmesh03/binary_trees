0ef09646c514f6a950fc0fdf17611e434a749635
girmesh03 <girmachewzewdie03@gmail.com>
2023-04-07 14:28:21 +0300

    16-binary_tree_is_perfect.c new update

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,81 +1,57 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_leaf - checks if a node is a leaf
- * @node: pointer to the node to check
- * Return: 1 if node is a leaf, otherwise 0
- */
-
-int binary_tree_is_leaf(const binary_tree_t *node)
-{
-	if (node == NULL)
-		return (0);
-	return (node->left == NULL && node->right == NULL);
-}
-
-/**
- * binary_tree_is_full - checks if a binary tree is full
- * @tree: pointer to the root node of the tree to check
- * Return: 1 if tree is full, otherwise 0
- */
-
-int binary_tree_is_full(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-		return (0);
-	if (tree->left == NULL && tree->right == NULL)
-		return (1);
-	if (tree->left != NULL && tree->right != NULL)
-		return (binary_tree_is_full(tree->left) &&
-				binary_tree_is_full(tree->right));
-	return (0);
-}
-
-/**
  * binary_tree_height - measures the height of a binary tree
  * @tree: pointer to the root node of the tree to measure the height.
  * Return: height of the tree
+ *
+ * Note: height of a tree is height of root node, height of a node is the
+ * the longest path from current node to leaf node.
+ * and height of a leaf node is 0
  */
-
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height = 0, right_height = 0;
 
-	if (tree == NULL)
+	/* height of an empty tree or a leaf node is 0 so return 0 */
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0);
 
-	if (tree->left != NULL)
-		left_height = binary_tree_height(tree->left) + 1;
+	/* else, recursively calculate the height of left and right subtrees */
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-	if (tree->right != NULL)
-		right_height = binary_tree_height(tree->right) + 1;
-
+	/* return the height of any node except leaf node and empty tree */
+	/* 1 is added to the height of the node to account the current node */
+	/* in which the height is being calculated */
 	if (left_height > right_height)
-		return (left_height);
+		return (1 + left_height);
 	else
-		return (right_height);
+		return (1 + right_height);
 }
 
 /**
  * binary_tree_is_perfect - checks if a binary tree is perfect
  * @tree: pointer to the root node of the tree to check
- * Return: 1 if tree is perfect, otherwise 0
+ * Return: 1 if tree is perfect, otherwise 0.
+ *
+ * A perfect binary tree is a binary tree in which all interior nodes have
+ * two children and all leaves have the same depth or same level.
  */
-
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
 
-	if (binary_tree_is_leaf(tree))
+	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 
 	if (tree->left != NULL && tree->right != NULL)
 	{
-		return (binary_tree_is_perfect(tree->left) &&
-				binary_tree_is_perfect(tree->right) &&
-				binary_tree_height(tree->left) ==
-					binary_tree_height(tree->right));
+		return ((binary_tree_is_perfect(tree->left) &&
+				 binary_tree_is_perfect(tree->right)) &&
+				(binary_tree_height(tree->left) ==
+				 binary_tree_height(tree->right)));
 	}
 	return (0);
 }
