a5accfc1ec3795ce3c638476c0bdf01de9b96045
girmesh03 <girmachewzewdie03@gmail.com>
2023-04-09 01:30:13 +0300

    111-bst_insert.c code replaced with recursive function

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -7,43 +7,38 @@
  */
 bst_t *bst_insert(bst_t **tree, int value)
 {
-	bst_t *new_node = NULL, *current;
-
+	bst_t *new_node = NULL;
+	/* if tree is not a valid pointer, return NULL */
 	if (!tree)
 		return (NULL);
-
+	/* if tree is empty, create a new node and return it */
 	if (!*tree)
 	{
 		*tree = binary_tree_node(NULL, value);
 		return (*tree);
 	}
-
-	current = *tree;
-	while (current)
+	/* no duplicates allowed, return NULL */
+	if (value == (*tree)->n)
+		return (NULL);
+	/* if value is less than the current node, go left */
+	if (value < (*tree)->n)
 	{
-		if (value == current->n)
-			return (NULL);
-
-		if (value < current->n)
-		{
-			if (!current->left)
-			{
-				new_node = binary_tree_node(current, value);
-				current->left = new_node;
-				break;
-			}
-			current = current->left;
-		}
-		else
-		{
-			if (!current->right)
-			{
-				new_node = binary_tree_node(current, value);
-				current->right = new_node;
-				break;
-			}
-			current = current->right;
-		}
+		/* if there is a left child, go to the next left child node */
+		if ((*tree)->left)
+			return (bst_insert(&(*tree)->left, value));
+		/* if there is no left child, create a new node and link it */
+		new_node = binary_tree_node(*tree, value);
+		(*tree)->left = new_node;
+	}
+	/* if value is greater than the current node, go right */
+	else if (value > (*tree)->n)
+	{
+		/* if there is a right child, go to the next right child node */
+		if ((*tree)->right)
+			return (bst_insert(&(*tree)->right, value));
+		/* if there is no right child, create a new node and link it */
+		new_node = binary_tree_node(*tree, value);
+		(*tree)->right = new_node;
 	}
 	return (new_node);
 }
